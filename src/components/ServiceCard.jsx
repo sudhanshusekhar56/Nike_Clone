9725c7cd9190d5d8d768e25cff6981775d21a723
Sudhanshu Sekhar <sudhanshusekhar56@hotmail.com>
2023-10-01 10:07:30 +0530

    Added Popular Products and Special Offer Section

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServiceCard = () => {
+  return <div>Service Card</div>;
+};
+
+export default ServiceCard;
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import { services } from "../constants";
 
 const Services = () => {
-  return <div></div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-8">
+      {/* {services.map((service) => {
+        <ServiceCard key={service.label} {...service} />;
+      })} */}
+    </section>
+  );
 };
 
 export default Services;
