ad426c232e55249bb70dfe73b89be9629f8e553f
avinashkranjan <ranjan.avinash@hotmail.com>
2023-10-01 23:05:07 +0530

    feat:minor fix

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
     <div>
-      <img src={imgURL} alt={label} /> {/* Display the imgURL as an image */}
-      <div>{subtext}</div> {/* Display the subtext */}
-      <div>{label}</div> {/* Display the label */}
+      <img src={imgURL} alt={label} />
+      <div>{subtext}</div>
+      <div>{label}</div>
     </div>
   );
 };
