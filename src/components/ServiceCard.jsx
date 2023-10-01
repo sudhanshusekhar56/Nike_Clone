20e14c5c435e41dd1f7f8566e52632d2dc0bdf4f
avinashkranjan <ranjan.avinash@hotmail.com>
2023-10-01 23:02:12 +0530

    feat:resolved

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-
 const ServiceCard = ({ imgURL, label, subtext }) => {
-  console.log(label);
-  return <div>{label}</div>;
+  return (
+    <div>
+      <img src={imgURL} alt={label} /> {/* Display the imgURL as an image */}
+      <div>{subtext}</div> {/* Display the subtext */}
+      <div>{label}</div> {/* Display the label */}
+    </div>
+  );
 };
 
 export default ServiceCard;
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,16 +1,18 @@
+import React from "react";
 import { services } from "../constants";
 import ServiceCard from "../components/ServiceCard";
 
 const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
-      {services.map((service) => {
+      {services.map((service) => (
         <ServiceCard
+          key={service.label}
           imgURL={service.imgURL}
           label={service.label}
           subtext={service.subtext}
-        />;
-      })}
+        />
+      ))}
     </section>
   );
 };
