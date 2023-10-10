f85aaf967ff2bf615beda582e5e65e722bc6827d
Sudhanshu Sekhar <sudhanshusekhar56@hotmail.com>
2023-10-10 22:41:40 +0530

    deployment

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-import Button from "./Button";
-import ShoeCard from "./ShoeCard";
-import Nav from "./Nav";
-import ReviewCard from "./ReviewCard";
-
-export { Button, ShoeCard, Nav, ReviewCard };
--- src/sections/Nav.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Nav = () => {
-  return <div>Hello, this is nav Section</div>;
-};
-
-export default Nav;
